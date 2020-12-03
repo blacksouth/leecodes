18ec74e0bfa11893e1ab99664735b02a7ce6a1fa
liuhaonan <873525217@qq.com>
2020-12-03 09:38:18 +0800

    tijiao

--- src/test/test02.java
+++ src/test/test02.java
@@ -4,11 +4,9 @@ import java.math.BigDecimal;
 
 public class test02 {
     public static void main(String[] args) {
-
-        System.out.println(new BigDecimal("10.00").multiply(new BigDecimal("1.000")).subtract(new BigDecimal("10.00")));
-
-        String a="enterprise.areaMeasure";
-         String b=a.substring(0,10);
-        System.out.println(b);
+        int n=34;
+        int m=12;
+        System.out.print(m++ +""+ ++n);
+        System.out.print(n++ +""+ ++m);
     }
 }
