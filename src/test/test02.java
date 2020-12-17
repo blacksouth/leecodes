12d53f1f229ce33cb3805888b6fe9cee97b9401d
“south” <873525217@qq.com>
2020-12-17 21:01:39 +0800

    最长共前缀

--- src/test/test02.java
+++ src/test/test02.java
@@ -1,16 +1,43 @@
 package test;
 
-import java.math.BigDecimal;
 
+/**
+ * 姓名 刘昊楠
+ * 电话 18008733159
+ *
+ * 若目标字符串中存在源字符串则返回第一个索引位置没有则返回-1
+ */
 public class test02 {
     public static void main(String[] args) {
         String a="asdqwezxc";
-        String b="{[()]}";
-        for (int i = 0; i <b.length() ; i++) {
-            System.out.println(b.substring(i,i+1));
-            System.out.println(b.substring(b.length()-i-1,10));
-        }
-
+        String b="zxc";
+        System.out.println(indexof(b,a));
 
     }
-}
+    /**
+     *
+     * @param source
+     * @param target
+     * @return
+     */
+    public static int indexof(String source,String target){
+        //目标字符串长度大于源字符串长度
+        if(target.length()<source.length()) return -1;
+        char[] arrayTarget = target.toCharArray();
+        char[] arraySource = source.toCharArray();
+        //flag 0表示源字符串还没有开始遍历 1表示已经开始遍历
+        int flag=0;
+        int temp=0;
+        for (int i = 0; i <arrayTarget.length ; i++) {
+            if((arrayTarget[i]==arraySource[temp])){
+                temp++;
+               if(temp==source.length()){
+                   return i-temp+1;
+               }
+               flag=1;
+            }else if(arrayTarget[i]!=arraySource[temp]&&flag==1)
+                 return -1;
+            }
+           return -1;
+        }
+ }
---
+++ src/test/test05.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test05 {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            if(i==5){
+                return;
+            }
+            System.out.println(i);
+        }
+        System.out.println(123);
+    }
+}
