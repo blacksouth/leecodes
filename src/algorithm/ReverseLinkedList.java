ee59aaa0e9f37d9f144ebe1647d86dd4861651f3
“south” <873525217@qq.com>
2022-01-10 20:17:42 +0800

    重复叠加字符串匹配

---
+++ src/algorithm/ReverseLinkedList.java
@@ -0,0 +1,77 @@
+package algorithm;
+
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 示例 3：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ *  
+ * <p>
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * <p>
+ */
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        ListNode listNode = reverseList(node1);
+        System.out.println(listNode);
+
+
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next;
+        ListNode curr = head;
+        while (curr.next != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
